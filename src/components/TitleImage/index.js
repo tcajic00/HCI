4d620fe4ec48fd923aa4c61b87314abf44cc60e9
tcajic00 <tcajic00@fesb.hr>
2020-10-29 12:22:55 +0100

    Creating TitleImage

--- src/components/TitleImage/index.js
+++ src/components/TitleImage/index.js
@@ -3,8 +3,12 @@ import React from 'react'
 import styles from './style.module.css'
  
 const TitleImage = () => (
-    <section className={styles.titleImage}>
-    </section>
+  <section className={styles.titleImage}>
+    <div>
+      <h1>VILLA OLIVA VERDE</h1>
+      <p>Isolated from the tourist crowd for a pleasant<br/> and peaceful vacation</p>
+    </div>
+  </section>
 )
  
 export default TitleImage
