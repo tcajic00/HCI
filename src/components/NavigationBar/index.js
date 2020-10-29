5779a7ff5f7a4c6f52b5394be8460f697e41150e
tcajic00 <tcajic00@fesb.hr>
2020-10-29 12:07:56 +0100

    Corrected navigationBar style reference (className)

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import {navs as navTabs} from '../../constants/const'
-import styles from './style.module.css'
-import { navigationBar } from './style.module.css'
- 
+import styles from './style.module.css' 
  
 const NavigationBar = ({ activeTab, useThisStyle }) => (
-  <nav className={styles[useThisStyle || navigationBar]}>
+  <nav className={styles[useThisStyle || 'navigationBar']}>
       {navTabs.map(tab => <li className={tab=== activeTab ? styles.active : ''}>
           {tab}</li>
       )}
