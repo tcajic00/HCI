3c6517502e259e2311bc211c024c365f23cd9bd8
tcajic00 <tcajic00@fesb.hr>
2020-11-06 12:23:16 +0100

    Insert FontAwesome

--- src/components/Combobox/index.js
+++ src/components/Combobox/index.js
@@ -1,25 +1,29 @@
 import React, {useState} from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
  
 import { languages } from '../../constants/const'
 import ComboboxItem from './ComboboxItem'
-import styles from './style.module.css' 
+import styles from './style.module.css'
  
 const Combobox = () => {
     const [selectedItem, setSelectedItem] = useState(
-        languages.find(el => el.name === "eng"))
+      languages.find(el => el.name === "eng"))
     const [open, setOpen] = useState(false)
     return (
-        <div className={styles.combobox} onClick={() => setOpen(!open) }>
-            {<ul style={{display: open ? 'flex' : 'none'}}>
-                {languages.map(el => 
-                <ComboboxItem {...el} 
-                    setSelected={setSelectedItem}
-                    selected={el.name === selectedItem.name}
-                />)}
-            </ul>}
-            {selectedItem.language}
-        </div>
+      <div className={styles.combobox} onClick={() => setOpen(!open) }>
+          {<ul style={{display: open ? 'flex' : 'none'}}>
+              {languages.map(el => 
+              <ComboboxItem {...el} 
+                setSelected={setSelectedItem}
+                selected={el.name === selectedItem.name}
+              />)}
+           </ul>
+          }
+          <FontAwesomeIcon icon={faChevronDown} size="0.2x" style={  {paddingRight: "12px"}} />
+          {selectedItem.language}
+      </div>
     )
 }
-
+ 
 export default Combobox
--- src/components/contactBar/index.js
+++ src/components/contactBar/index.js
@@ -1,11 +1,16 @@
-import React from "react"
-import styles from "./style.module.css"
-
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons'
+ 
+import styles from './style.module.css'
+ 
 const ContactBar = () => (
-  <main className={styles.container}>
-    <span>example@email.com</span>
-    <span>+0385911122334</span>
-  </main>
+    <main className={styles.container}>
+       <span>example@email.com</span>
+       <FontAwesomeIcon icon={faEnvelope} color="white" />
+       <span>+0385911122334</span>
+       <FontAwesomeIcon icon={faPhone} color="white"/>
+    </main>
 )
-
+ 
 export default ContactBar
