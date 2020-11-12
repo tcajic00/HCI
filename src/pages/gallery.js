b6166d22de8b67d3a94ddc8adeec5df6c447422c
tcajic00 <tcajic00@fesb.hr>
2020-11-12 13:13:42 +0100

    adding GalleryContainer

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,10 +1,12 @@
 import React from "react"
 import HeaderFooterLayout from "../layouts/headerFooter"
+import GalleryContent from "../modules/GalleryContent"
 import SeparatorBar from '../modules/SeparatorBar'
  
 const PhotoGallery = () => (
     <HeaderFooterLayout activeTab="Photo Gallery">
         <SeparatorBar text="Photo Gallery"/>
+        <GalleryContent />
     </HeaderFooterLayout>
 )
  
