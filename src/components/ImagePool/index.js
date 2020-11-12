6486e834168c987a8ded4272b56be0b9fd6052f9
tcajic00 <tcajic00@fesb.hr>
2020-11-12 13:40:56 +0100

    Correcting styling

--- src/components/ImagePool/index.js
+++ src/components/ImagePool/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
+import styles from './style.module.css'
  
 const ImagePool = ({start, end}) => {
     const data = useStaticQuery(graphql`
@@ -21,7 +22,7 @@ const ImagePool = ({start, end}) => {
         }
     `)
     return (
-    <div>
+    <div className={styles.imagePool}>
         {data.myImages.edges.slice(start,end).map(edge => 
           <Img fixed={edge.node.localFile.childImageSharp.fixed}/>
         )}
